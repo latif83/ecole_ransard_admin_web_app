aa3a42b975b1ec6a6066112c5a3a912cc220987c
latif83 <latifm8360@gmail.com>
2024-08-07 15:19:32 +0000

    Updates!

--- app/teacher/page.jsx
+++ app/teacher/page.jsx
@@ -1,45 +1,164 @@
-export default function Overview() {
-    return (
-      <div className="w-full">
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-          {/* Total Number of Students */}
-          <div className="rounded-lg bg-red-500 p-4 shadow-md">
-            <h2 className="text-lg font-semibold text-white">Total Students</h2>
-            <p className="mt-2 text-2xl font-bold text-white">120</p>
-          </div>
-          
-          {/* Total Number of Parents */}
-          <div className="rounded-lg bg-green-500 p-4 shadow-md">
-            <h2 className="text-lg font-semibold text-white">Total Parents</h2>
-            <p className="mt-2 text-2xl font-bold text-white">80</p>
-          </div>
-          
-          {/* Total Number of Classes */}
-          <div className="rounded-lg bg-blue-500 p-4 shadow-md">
-            <h2 className="text-lg font-semibold text-white">Total Classes</h2>
-            <p className="mt-2 text-2xl font-bold text-white">10</p>
-          </div>
-          
+"use client"
+import Image from "next/image";
+import { Fragment, useEffect, useState } from "react";
+import Link from "next/link";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSpinner } from "@fortawesome/free-solid-svg-icons";
+
+export default function TeacherDashboard() {
+
+  const [loading, setLoading] = useState(false)
+
+  const [sections, setSections] = useState([])
+
+//   useEffect(() => {
+
+//     const getAssignedCourses = async () => {
+//       setLoading(true)
+//       try {
+
+//         const accessToken = localStorage.getItem("SMSTOKEN")
+
+//         const getTokenData = await verifyToken(accessToken)
+//         console.log(getTokenData)
+//         const teacherId = getTokenData?.decodedToken?.id
+
+//         const response = await fetch(`${serverUrl}/teacher/assigned_classes/${teacherId}`, {
+//           method: "GET",
+//           headers: {
+//             Authorization: `Bearer ${accessToken}`
+//           }
+//         })
+
+//         const responseData = await response.json()
+
+//         if (!response.ok) {
+//           showToast("error", responseData.message)
+//           return
+//         }
+
+//         setSections(responseData.assignedClasses)
+
+//       } catch (err) {
+//         console.log(err)
+//       } finally {
+//         setLoading(false)
+//       }
+
+
+//     }
+
+//     getAssignedCourses()
+
+//   }, [])
+
+  return (
+    <div>
+      <div className="sm:px-10 px-2 py-5 bg-gray-600">
+
+        <div className="flex justify-between">
+          <div className="flex gap-1.5 justify-center items-center">
+            <Image
+              width={500}
+              height={500}
+              style={{ width: "50px", height: "50px" }}
+              className="object-cover rounded-full"
+              src={"/logo.png"}
+              alt="Logo"
+            />
+            <div>
+              <span className="text-white font-bold">
+                ECOLE RONSARD
+              </span>
+            </div>
+
+            </div>
+            
+            <div>
+            <button className="inline-flex w-full justify-center text-white gap-x-1.5 rounded-t-md px-3 py-2 text-sm">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
+                </svg>
+
+                Abdul-Latif Mohammed
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="-mr-1 h-5 w-5">
+  <path strokeLinecap="round" strokeLinejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
+</svg>
+            </button>
+
         
-        
+          </div>
+
+          
+
         </div>
 
-        {/* Attendance Summary */}
-        <div className="mt-10">
-            <h2 className="text-lg font-semibold text-gray-800 border-b-2">Attendance Summary</h2>
-            <p className="mt-2 text-2xl font-bold text-gray-700">95% Attendance Today</p>
+
+        <div className="grid grid-cols-2 gap-2 items-center mt-8 text-gray-100">
+
+          <div>
+            <h1 className="text-sm font-bold">
+              Welcome to the,
+            </h1>
+            <p className="text-2xl font-bold">
+              Teacher's Dashboard
+            </p>
+
           </div>
 
-          {/* Upcoming Events */}
-          <div className="mt-10">
-            <h2 className="text-lg font-semibold text-gray-800 border-b-2">Upcoming Events</h2>
-            <ul className="mt-2 text-gray-700">
-              <li>- Parent-Teacher Meeting on July 20th</li>
-              <li>- School Sports Day on August 10th</li>
-              <li>- Science Fair on September 5th</li>
-            </ul>
-          </div>
+          <form class="w-full mx-auto">
+            <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only">Search</label>
+            <div class="relative">
+              <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                <svg class="w-4 h-4 text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
+                </svg>
+              </div>
+              <input type="search" id="default-search" class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500" placeholder="Search Assigned Classes..." required />
+              <button type="submit" class="text-white absolute end-2.5 bottom-2.5 bg-gray-700 hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2">Search</button>
+            </div>
+          </form>
+        </div>
+
+
       </div>
-    );
-  }
-  
+
+      <div className="sm:px-10 px-2 py-5">
+
+        <h1 className="text-xl font-bold">
+          Assigned Classes
+        </h1>
+        <p className="text-sm p-0 m-0">
+          Click on any of the assigned classes to manage!
+        </p>
+
+
+
+        {loading ? <div className="mt-3 flex flex-col justify-center items-center gap-2">
+          <FontAwesomeIcon spin icon={faSpinner} width={30} height={30} />
+          <p>Loading data...</p>
+        </div> : sections.length < 1 ? <div className="mt-3 flex flex-col justify-center items-center gap-2">
+          <p>No Classes Assigned!</p>
+        </div> : <div className="grid grid-cols-4 gap-4 mt-3">
+
+
+
+          {sections.map((session) => (<div className="border shadow-md p-2 rounded-lg">
+            <p>{session.classSection}</p>
+
+            <div className="flex justify-end">
+              <Link href={`/t/dashboard/${session.classSectionId}`} className="p-1.5 rounded bg-blue-500 text-gray-100 hover:bg-blue-600 mt-2 flex items-center gap-1.5">
+                <span>Manage</span> <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3" />
+                </svg>
+
+              </Link>
+            </div>
+          </div>))}
+
+        </div>}
+
+      </div>
+    </div>
+  );
+}
