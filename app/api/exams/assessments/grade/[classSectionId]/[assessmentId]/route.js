ae0a6fe9e2665024337edb763c95a5d3a2fb816e
latif83 <latifm8360@gmail.com>
2024-08-12 11:03:21 +0000

    Updates!

---
+++ app/api/exams/assessments/grade/[classSectionId]/[assessmentId]/route.js
@@ -0,0 +1,128 @@
+import prisma from "@/config/prisma";
+import { NextResponse } from "next/server";
+
+export async function GET(req, { params }) {
+  try {
+    const { classSectionId, assessmentId } = params;
+
+    if (!classSectionId) {
+      return NextResponse.json(
+        { message: "Class section ID is required" },
+        { status: 400 }
+      );
+    }
+
+    const students = await prisma.Students.findMany({
+      where: {
+        classSectionsId: classSectionId,
+      },
+      include: {
+        Grades: {
+          where: {
+            assessmentId,
+          },
+        },
+      },
+    });
+
+    const assessment = await prisma.Assessments.findUnique({
+      where: {
+        id: assessmentId,
+      },
+      include: {
+        subject: true,
+      },
+    });
+
+    const formattedResponse = students.map((student) => {
+      const grade = student.Grades[0];
+      const studentName = `${student.firstName} ${student.lastName}`;
+      const score = grade?.score ? grade?.score : "N/A";
+      const weight =
+        score != "N/A" ? (score / assessment.marks) * assessment.weight : "N/A";
+
+      return {
+        studentName,
+        score,
+        weight,
+      };
+    });
+
+    const response = {
+      subject: assessment.subject.name,
+      assessment: assessment.name,
+      marks: assessment.marks,
+      weight: assessment.weight,
+      students: formattedResponse,
+    };
+
+    return NextResponse.json(response, { status: 200 });
+  } catch (error) {
+    console.error("Error fetching students:", error);
+    return NextResponse.json(
+      { message: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
+}
+
+export async function POST(req, { params }) {
+  try {
+    const { score, studentId } = await req.json();
+
+    const { assessmentId } = params;
+
+    if (!assessmentId || !studentId) {
+      return NextResponse.json({ message: "Assessment ID and student ID are required" }, { status: 400 });
+    }
+
+    if (!score) {
+      return NextResponse.json({ message: "Score is required" }, { status: 400 });
+    }
+
+    const assessment = await prisma.Assessments.findUnique({
+      where: {
+        id: assessmentId,
+      },
+    });
+
+    if (!assessment) {
+      return NextResponse.json({ message: "Assessment not found" }, { status: 404 });
+    }
+
+    if (score > assessment.marks) {
+      return NextResponse.json({ message: "Score cannot be greater than the assessment's total marks" }, { status: 400 });
+    }
+
+    const existingGrade = await prisma.Grades.findFirst({
+      where: {
+        assessmentId,
+        studentId,
+      },
+    });
+
+    if (existingGrade) {
+      await prisma.Grades.update({
+        where: {
+          id: existingGrade.id,
+        },
+        data: {
+          score,
+        },
+      });
+    } else {
+      await prisma.Grades.create({
+        data: {
+          assessmentId,
+          studentId,
+          score,
+        },
+      });
+    }
+
+    return NextResponse.json({ message: "Student scored successfully" }, { status: 201 });
+  } catch (error) {
+    console.error("Error scoring student:", error);
+    return NextResponse.json({ message: "Internal Server Error" }, { status: 500 });
+  }
+}
--- app/teacher/dashboard/[classSectionId]/assessments/[subjectId]/[assessmentId]/page.jsx
+++ app/teacher/dashboard/[classSectionId]/assessments/[subjectId]/[assessmentId]/page.jsx
@@ -1,7 +1,41 @@
+"use client"
 import { faArrowLeftLong, faSpinner } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useEffect, useState } from "react";
+import { toast } from "react-toastify";
 
-export default function () {
+export default function ({params}) {
+
+    const {classSectionId,assessmentId} = params
+
+    const [students,setStudents] = useState([])
+    const [data,setData] = useState({})
+    const [loading,setLoading] = useState(false)
+
+    useEffect(()=>{
+        const getGrades = async()=>{
+            setLoading(true)
+            try{
+                const response = await fetch(`/api/exams/assessments/grade/${classSectionId}/${assessmentId}`)
+                const responseData = await response.json()
+                if(!response.ok){
+                    toast.error(responseData.message)
+                    return
+                }
+
+                setStudents(responseData.students)
+                setData(responseData)
+            }
+            catch(e){
+                console.log(e)
+            } finally{
+                setLoading(false)
+            }
+        }
+
+        getGrades()
+    },[])
+
     return (
         <div>
 
@@ -17,14 +51,14 @@ export default function () {
                 </div>
 
                 <div className="flex gap-10 my-2">
-                    <div>
+                    <div className="text-center">
                         <h2 className="text-xl font-semibold">Subject</h2>
-                        <p className="text-xs">Mathematics</p>
+                        <p className="text-xs"> {loading ? <FontAwesomeIcon icon={faSpinner} spin /> : data.subject} </p>
                     </div>
 
-                    <div>
+                    <div className="text-center">
                         <h2 className="text-xl font-semibold">Assessment</h2>
-                        <p className="text-xs">Mid-Term Exams</p>
+                        <p className="text-xs">{loading ? <FontAwesomeIcon icon={faSpinner} spin /> : data.assessment}</p>
                     </div>
                 </div>
 
@@ -39,11 +73,11 @@ export default function () {
                             </th>
                             <th scope="col" className="px-6 py-3">
                                 <p>Score</p>
-                                <p className="text-xs text-red-600">50</p>
+                                <p className="text-xs text-red-600">[ {loading ? <FontAwesomeIcon icon={faSpinner} spin /> : data.marks} ]</p>
                             </th>
                             <th scope="col" className="px-6 py-3">
                                 <p>Weight</p>
-                                <p className="text-xs text-red-600">20%</p>
+                                <p className="text-xs text-red-600">[ {loading ? <FontAwesomeIcon icon={faSpinner} spin /> : data.weight}% ]</p>
                             </th>
                             <th scope="col" className="px-6 py-3">
                                 Actions
@@ -52,31 +86,7 @@ export default function () {
                     </thead>
                     <tbody>
 
-                        <tr className="bg-white border-b hover:bg-gray-50">
-                            <th className="px-6 py-4">Daniel Osei</th>
-                            <td className="px-6 py-4"> 30 </td>
-                            <td className="px-6 py-4"> 15% </td>
-                            <td className="px-6 py-4 flex gap-4 justify-center items-center">
-                                <span className="font-medium text-blue-600 hover:underline"
-                                >
-                                    Score
-                                </span>
-                            </td>
-                        </tr>
-
-                        <tr className="bg-white border-b hover:bg-gray-50">
-                            <th className="px-6 py-4">Bismark Agyei</th>
-                            <td className="px-6 py-4"> 32 </td>
-                            <td className="px-6 py-4"> 17% </td>
-                            <td className="px-6 py-4 flex gap-4 justify-center items-center">
-                                <span className="font-medium text-blue-600 hover:underline"
-                                >
-                                    Score
-                                </span>
-                            </td>
-                        </tr>
-
-                        {/* {assessmentsLoading ? (
+                        {loading ? (
                                 <tr className="bg-white border-b hover:bg-gray-50">
                                     <td className="px-6 py-4 text-center" colSpan={4}>
                                         <FontAwesomeIcon
@@ -87,51 +97,27 @@ export default function () {
                                         />
                                     </td>
                                 </tr>
-                            ) : assessments.length > 0 ? (
-                                assessments.map((assesment) => (
+                            ) : students.length > 0 ? (
+                                students.map((student) => (
                                     <tr className="bg-white border-b hover:bg-gray-50">
-                                        <th className="px-6 py-4">{assesment.name}</th>
-                                        <td className="px-6 py-4"> {assesment.description ? assesment.description : 'N/A'} </td>
-                                        <td className="px-6 py-4"> {assesment.marks} </td>
-                                        <td className="px-6 py-4"> {assesment.weight}% </td>
-                                        <td className="px-6 py-4 flex gap-4 items-center">
-                                            <Link
-                                                href={`/teacher/dashboard/${classSectionId}/assessments/${subjectId}/${assesment.id}`}
-                                                className="font-medium text-blue-600 hover:underline"
-                                            >
-                                                Score Students
-                                            </Link>
-
-                                            <div className="flex gap-2">
-                                                <svg onClick={() => {
-                                                    setAssessmentData(assesment);
-                                                    setEditAssessment(true);
-                                                }}
-                                                    xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 cursor-pointer bg-blue-600 hover:bg-blue-400 text-white p-1 rounded-md">
-                                                    <path strokeLinecap="round" strokeLinejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
-                                                </svg>
-
-                                                <svg
-                                                    onClick={() => {
-                                                        setSelectedAssessmentId(assesment.id);
-                                                        setDelAssessment(true);
-                                                    }}
-                                                    xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 cursor-pointer bg-red-600 hover:bg-red-400 text-white p-1 rounded-md">
-                                                    <path strokeLinecap="round" strokeLinejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
-                                                </svg>
-
-
-                                            </div>
-                                        </td>
-                                    </tr>
+                                    <th className="px-6 py-4">{student.studentName}</th>
+                                    <td className="px-6 py-4"> {student.score} </td>
+                                    <td className="px-6 py-4"> {student.weight}% </td>
+                                    <td className="px-6 py-4 flex gap-4 justify-center items-center">
+                                        <span className="font-medium text-blue-600 hover:underline"
+                                        >
+                                            Score
+                                        </span>
+                                    </td>
+                                </tr>
                                 ))
                             ) : (
                                 <tr className="bg-white border-b hover:bg-gray-50">
-                                    <td className="px-6 py-4" colSpan={5}>
-                                        No Assessments found
+                                    <td className="px-6 py-4" colSpan={4}>
+                                        No Students found
                                     </td>
                                 </tr>
-                            )} */}
+                            )}
                     </tbody>
                 </table>
             </div>
