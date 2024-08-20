332e1f7c3015a3f60c69708bb27d872f7fa41b0a
latif83 <latifm8360@gmail.com>
2024-08-20 20:08:03 +0000

    Updates!

--- app/parents/billing/page.jsx
+++ app/parents/billing/page.jsx
@@ -1,5 +1,5 @@
 // Billing Tab Component
-export default function BillingTab = () => {
+export default function BillingTab ()  {
     return (
       <div>
         <h3 className="text-lg font-semibold mb-4 text-blue-600">Billing</h3>
