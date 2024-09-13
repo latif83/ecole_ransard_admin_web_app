bec2765893741de04e2adfb2f2d1049a25953d02
latif83 <latifm8360@gmail.com>
2024-09-13 17:42:54 +0000

    Updates on default password reset!

--- app/reset/page.jsx
+++ app/reset/page.jsx
@@ -2,7 +2,7 @@
 import { faSpinner } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useRouter } from 'next/navigation';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { toast } from 'react-toastify';
 
 export default function ResetPassword() {
@@ -16,47 +16,60 @@ export default function ResetPassword() {
 
     const router = useRouter()
 
+    const [rPass, setRPass] = useState(false)
+
     const handlePasswordChange = async () => {
-        try {
+        setRPass(true)
+    };
 
-            setLoading(true)
+    useEffect(() => {
 
-            // Check if new passwords match
-            if (newPassword !== confirmPassword) {
-                toast.error('Passwords do not match.');
-                return;
+        const resetPassword = async () => {
+            try {
+
+                setLoading(true)
+
+                // Check if new passwords match
+                if (newPassword !== confirmPassword) {
+                    toast.error('Passwords do not match.');
+                    return;
+                }
+
+                const data = {
+                    password: newPassword,
+                    userIdentity: localStorage.getItem("identity"),
+                    role: localStorage.getItem("loggedInRole")
+                }
+
+                const response = await fetch(`/api/reset`, {
+                    method: "POST",
+                    "Content-Type": "application/json",
+                    body: JSON.stringify(data)
+                })
+
+                const responseData = await response.json()
+
+                if (!response.ok) {
+                    toast.error(responseData.error)
+                    return
+                }
+
+                toast.success(responseData.message)
+
+                router.replace("/")
+
+            } catch (e) {
+                console.log(e)
+            } finally {
+                setLoading(false)
             }
-
-            const data = {
-                password: newPassword,
-                userIdentity: localStorage.getItem("identity"),
-                role: localStorage.getItem("loggedInRole")
-            }
-
-            const response = await fetch(`/api/reset`, {
-                method: "POST",
-                "Content-Type": "application/json",
-                body: JSON.stringify(data)
-            })
-
-            const responseData = await response.json()
-
-            if (!response.ok) {
-                toast.error(responseData.error)
-                return
-            }
-
-            toast.success(responseData.message)
-
-            router.replace("/")
-
-        } catch (e) {
-            console.log(e)
-        } finally {
-            setLoading(false)
         }
 
-    };
+        if (rPass) {
+            resetPassword()
+        }
+
+    }, [rPass])
 
     return (
         <div className="flex flex-col gap-6 items-center justify-center min-h-screen bg-blue-800">
@@ -135,6 +148,7 @@ export default function ResetPassword() {
                             {loading ? <span className='flex gap-1.5 items-center'> <FontAwesomeIcon icon={faSpinner} spin /> <span>Reset Password</span> </span> : "Reset Password"}
                         </button>
                         <button
+                        type='button'
                             onClick={() => router.replace("/")}
                             className="w-full py-2 px-4 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                         >
